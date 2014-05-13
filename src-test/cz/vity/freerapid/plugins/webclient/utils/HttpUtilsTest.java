5b5f6feb7567b09d9e41d29563a4c33cc0db348b
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 05:09:52 +0530

    SVN Update# 1168 by Vity: +tests for Utils

---
+++ src-test/cz/vity/freerapid/plugins/webclient/utils/HttpUtilsTest.java
@@ -0,0 +1,59 @@
+package cz.vity.freerapid.plugins.webclient.utils;
+
+import cz.vity.freerapid.utilities.Utils;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Junit test for HttpUtils
+ *
+ * @author Ladislav Vitasek
+ */
+public class HttpUtilsTest {
+    private boolean isWindows;
+
+    @Before
+    public void before() {
+        isWindows = Utils.isWindows();
+    }
+
+    @Test
+    public void testGetFileName() {
+        final String s = HttpUtils.replaceInvalidCharsForFileSystem("diskFileName:", "_");
+        if (isWindows) {
+            Assert.assertEquals("Replace invalid chars for file system on Windows", "diskFileName_", s);
+        } else {
+            Assert.assertEquals("Replace invalid chars for file system on other file system", "diskFileName:", s);
+        }
+    }
+
+    @Test
+    public void testReplaceInvalidCharsForFileSystem() {
+        final TestPostMethod postMethod = new TestPostMethod();
+        postMethod.responseHeader = new Header("Content-Disposition", "Content-Disposition: =?UTF-8?attachment;filename=\"Two Peaks Personal Vehicle Manager 2005 3.2.zip\";?=");
+        String s = HttpUtils.getFileName(postMethod);
+        Assert.assertEquals("Extracting file name #1", "Two Peaks Personal Vehicle Manager 2005 3.2.zip", s);
+        postMethod.responseHeader = new Header("Content-Disposition", "Content-Disposition: attachment;filename=Two Peaks Personal Vehicle Manager 2005 3.2.zip");
+        s = HttpUtils.getFileName(postMethod);
+        Assert.assertEquals("Extracting file name #2", "Two Peaks Personal Vehicle Manager 2005 3.2.zip", s);
+    }
+
+    private static class TestPostMethod extends PostMethod {
+        public Header responseHeader;
+
+        public TestPostMethod() {
+            super("http://localhost");
+        }
+
+        @Override
+        public Header getResponseHeader(String s) {
+
+            if (responseHeader.getName().equalsIgnoreCase(s))
+                return responseHeader;
+            return super.getResponseHeader(s);
+        }
+    }
+}
---
+++ src-test/cz/vity/freerapid/plugins/webclient/utils/PlugUtilsTest.java
@@ -0,0 +1,90 @@
+package cz.vity.freerapid.plugins.webclient.utils;
+
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * Junit test for PlugUtils
+ *
+ * @author Ladislav Vitasek
+ */
+public class PlugUtilsTest {
+    @Before
+    public void setUp() {
+        // Add your code here
+    }
+
+    @After
+    public void tearDown() {
+        // Add your code here
+    }
+
+    @Test
+    public void testGetFileSizeFromString() {
+        assertEquals("File size from MB", PlugUtils.getFileSizeFromString("\t  2500 MB"), 2500 * 1024 * 1024L);
+        assertEquals("File size from GB", PlugUtils.getFileSizeFromString("  2 \t500 GB"), 2500 * 1024L * 1024 * 1024L);
+        assertEquals("File size from bytes", PlugUtils.getFileSizeFromString(" 2500 byTes"), 2500);
+        assertEquals("File size with dot in a number", PlugUtils.getFileSizeFromString("25.00 kb"), new BigDecimal("25.00").multiply(BigDecimal.valueOf(1024L)).setScale(0, RoundingMode.UP).longValue());
+        assertEquals("File size remove spaces", PlugUtils.getFileSizeFromString("   25     kB   "), 25 * 1024);
+        assertEquals("File size with dot in a number #2", PlugUtils.getFileSizeFromString("25,75 MB"), new BigDecimal("25.75").multiply(BigDecimal.valueOf(1024 * 1024L)).setScale(0, RoundingMode.UP).longValue());
+
+    }
+
+    @Test
+    public void testMatcher() {
+        assertEquals(PlugUtils.matcher("test", "test").matches(), true);
+    }
+
+    @Test
+    public void testFind() {
+        assertEquals(PlugUtils.find(".*?test.*?", "xxxtestyyy"), true);
+    }
+
+    @Test
+    public void testUnescapeHtml() {
+        assertEquals(PlugUtils.unescapeHtml("&nbsp;&nbsp;&gt;&lt;"), "  ><");
+    }
+
+    @Test
+    public void testGetParameter() throws PluginImplementationException {
+        try {
+            assertEquals(PlugUtils.getParameter("par", "\"input type=\"hidden\" name=\"par\" value=val>"), "val");
+            fail("Should throw exception - missing quotes");
+        } catch (PluginImplementationException e) {
+
+        }
+        assertEquals(PlugUtils.getParameter("par", "\"input type=\"hidden\" name=\"par\" value=\"val\">"), "val");
+    }
+
+    @Test
+    public void testAddParameters() throws PluginImplementationException {
+        PostMethod postMethod = new PostMethod("http://localhost");
+        try {
+            PlugUtils.addParameters(postMethod, "\"input type=\"hidden\" name=\"par\" value=\"val>", new String[]{"par"});
+            fail("Should throw exception");
+        } catch (PluginImplementationException e) {
+
+        }
+        postMethod = new PostMethod("http://localhost");
+        PlugUtils.addParameters(postMethod, "\"input type=\"hidden\" name=\"par\" value=\"val\">", new String[]{"par"});
+        assertEquals(postMethod.getParameter("par").getValue(), "val");
+    }
+
+    @Test
+    public void testReplaceEntities() {
+        assertEquals(PlugUtils.replaceEntities("asdasd &amp; fdsdfsdf"), "asdasd & fdsdfsdf");
+    }
+
+    @Test
+    public void testRecognize() {
+        //
+    }
+}
