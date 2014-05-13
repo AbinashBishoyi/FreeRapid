cb98b9b2ef55455a22e90eaf8ae67e6cd3f2eef7
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 04:16:39 +0530

    SVN Update# 663 by Vity: plug utils - entities

--- src/cz/vity/freerapid/plugins/webclient/PlugUtils.java
+++ src/cz/vity/freerapid/plugins/webclient/PlugUtils.java
@@ -1,6 +1,13 @@
 package cz.vity.freerapid.plugins.webclient;
 
+import cz.vity.freerapid.plugins.webclient.utils.Entities;
+
+import java.io.IOException;
+import java.io.StringWriter;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,6 +15,8 @@ import java.util.regex.Pattern;
  * @author Ladislav Vitasek
  */
 public final class PlugUtils {
+    private final static Logger logger = Logger.getLogger(PlugUtils.class.getName());
+
 
     /**
      * Parse input string and converts it into bytes
@@ -38,7 +47,7 @@ public final class PlugUtils {
         }
         value = value.trim().replaceAll(" ", "").replaceAll(",", ".");
         if (value.indexOf('.') > 0)
-            return new BigDecimal(value).multiply(new BigDecimal(constant)).setScale(0).longValue();
+            return new BigDecimal(value).multiply(BigDecimal.valueOf(constant)).setScale(0, RoundingMode.HALF_EVEN).longValue();
         else
             return Long.parseLong(value) * constant;
     }
@@ -52,4 +61,35 @@ public final class PlugUtils {
     public static boolean find(final String regexp, final String contentString) {
         return matcher(regexp, contentString).find();
     }
+
+
+    /**
+     * <p>Unescapes a string containing entity escapes to a string
+     * containing the actual Unicode characters corresponding to the
+     * escapes. Supports HTML 4.0 entities.</p>
+     * <p/>
+     * <p>For example, the string "&amp;lt;Fran&amp;ccedil;ais&amp;gt;"
+     * will become "&lt;Fran&ccedil;ais&gt;"</p>
+     * <p/>
+     * <p>If an entity is unrecognized, it is left alone, and inserted
+     * verbatim into the result string. e.g. "&amp;gt;&amp;zzzz;x" will
+     * become "&gt;&amp;zzzz;x".</p>
+     *
+     * @param str the <code>String</code> to unescape, may be null
+     * @return a new unescaped <code>String</code>, <code>null</code> if null string input
+     */
+    public static String unescapeHtml(String str) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            StringWriter writer = new StringWriter((int) (str.length() * 1.5));
+            Entities.HTML40.unescape(writer, str);
+            return writer.toString();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Cannot unescape HTML", e);
+            return null;
+        }
+    }
+
 }
