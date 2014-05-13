31cb9f0016fe8cee7a4cc3ba4c627b97f29e85cf
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 05:02:15 +0530

    SVN Update# 1095 by Vity: UIStringsManager - English version, now reads language version properly

--- src/cz/vity/freerapid/core/UIStringsManager.java
+++ src/cz/vity/freerapid/core/UIStringsManager.java
@@ -4,21 +4,38 @@ import org.jdesktop.application.ResourceManager;
 import org.jdesktop.application.ResourceMap;
 
 import javax.swing.*;
+import java.util.List;
+import java.util.ResourceBundle;
 
 /**
+ * Creates localization for Java internal SWING strings
+ *
  * @author Vity
  */
 final public class UIStringsManager {
     private static boolean loaded = false;
 
+    /**
+     * Do not instantiate UIStringsManager.
+     */
+    private UIStringsManager() {
+    }
+
+    /**
+     * Loads strings for associated by current locale into UIManager
+     *
+     * @param manager resource manager for creating resource maps
+     * @see javax.swing.UIManager
+     */
     public static void load(ResourceManager manager) {
         if (loaded)
             return;
 
         final ResourceMap map = manager.getResourceMap(UIStringsManager.class);
-
-        for (String key : map.getBundleNames()) {
-            UIManager.put(key, map.getString(key));
+        final List<String> stringList = map.getBundleNames();
+        final ResourceBundle bundle = ResourceBundle.getBundle(stringList.get(0));
+        for (String key : bundle.keySet()) {
+            UIManager.put(key, bundle.getString(key));
         }
         loaded = true;
     }
