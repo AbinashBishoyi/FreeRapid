25a94ccb51f71474c8c029edf91f02d3708a1795
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 05:11:19 +0530

    SVN Update# 1181 by Vity: +upgrade version +copy paste actions

--- src/cz/vity/freerapid/core/Consts.java
+++ src/cz/vity/freerapid/core/Consts.java
@@ -15,7 +15,7 @@ public class Consts {
     /**
      * Version
      */
-    public static final String VERSION = "0.8a";
+    public static final String VERSION = "0.8";
 
     /**
      * verze a jmeno programu
