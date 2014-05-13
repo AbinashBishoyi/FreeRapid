827be79dbc6393936d66c369421f89b9ddfe9fb4
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 04:09:06 +0530

    SVN Update# 604 by Vity: sprava chyby, pri vyjimce setLength na RandomAccessFile

--- src/cz/vity/freerapid/utilities/FileUtils.java
+++ src/cz/vity/freerapid/utilities/FileUtils.java
@@ -18,16 +18,19 @@ public class FileUtils {
      *
      * @param f    soubor
      * @param size velikost
+     * @param task running task
      * @return soubor pro zapis nahodnych dat - nastaveny na zacatek
      * @throws IOException vyjimka pri IO
      */
     public static OutputStream createEmptyFile(final File f, final long size, DownloadTask task) throws IOException {
         final int megabyte = 1024 * 1024;
         long written = 0;
-        FileOutputStream fos = null;
+        RandomAccessFile fos;
         final byte[] bytes = new byte[megabyte];
+        fos = null;
         try {
-            fos = new FileOutputStream(f);
+            fos = new RandomAccessFile(f, "rw");
+            fos.setLength(size);
             int toWrite = megabyte;
             while (written != size) {
                 if (task.isTerminated())
@@ -38,12 +41,38 @@ public class FileUtils {
                 fos.write(bytes, 0, toWrite);
                 written += toWrite;
             }
-        } finally {
+        } catch (IOException e) {
             if (fos != null)
                 fos.close();
+            throw e;
         }
 
-        return new FileOutputStream(new RandomAccessFile(f, "rw").getFD());
+        fos.seek(0);
+        return new FileOutputStream(fos.getFD());
     }
+//    public static OutputStream createEmptyFile(final File f, final long size, DownloadTask task) throws IOException {
+//        final int megabyte = 1024 * 1024;
+//        long written = 0;
+//        FileOutputStream fos = null;
+//        final byte[] bytes = new byte[megabyte];
+//        try {
+//            fos = new FileOutputStream(f);
+//            int toWrite = megabyte;
+//            while (written != size) {
+//                if (task.isTerminated())
+//                    return null;
+//                if (size - written < megabyte) {
+//                    toWrite = (int) (size - written);
+//                }
+//                fos.write(bytes, 0, toWrite);
+//                written += toWrite;
+//            }
+//        } finally {
+//            if (fos != null)
+//                fos.close();
+//        }
+//
+//        return new FileOutputStream(new RandomAccessFile(f, "rw").getFD());
+//    }
 
 }
