e6e59a581f00c96f2a2e0fd0f9af4d9d5d533c15
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 04:24:43 +0530

    SVN Update# 736 by Vity: sjednoceni properties upgrade na verzi 0.7

--- src/cz/vity/freerapid/core/Consts.java
+++ src/cz/vity/freerapid/core/Consts.java
@@ -15,7 +15,7 @@ public class Consts {
     /**
      * Version
      */
-    public static final String VERSION = "0.65";
+    public static final String VERSION = "0.7";
 
     /**
      * verze a jmeno programu
