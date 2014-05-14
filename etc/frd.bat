84edda49481eaf1e9d5950e4dbeafce47b5cbf16
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 09:35:46 +0530

    SVN Update# 4153 by ntoskrnl: improve batch script

---
+++ etc/frd.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Try this batch file if you encounter problems launching FRD with frd.exe
+REM (usually some security application is blocking it for unknown reasons).
+start javaw.exe -Xmx192m -jar frd.jar
