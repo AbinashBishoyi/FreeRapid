6de437fa2e272da1f431b07aa68bd5270a689b94
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 10:08:51 +0530

    SVN Update# 4582 by ntoskrnl: FileTypeIconProvider cleanups and improvements

--- src/cz/vity/freerapid/core/FileTypeIconProvider.java
+++ src/cz/vity/freerapid/core/FileTypeIconProvider.java
@@ -10,9 +10,9 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.Hashtable;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,50 +21,30 @@ import java.util.regex.Pattern;
  * @author Vity
  */
 public class FileTypeIconProvider {
-    private ResourceMap map;
     private final static Logger logger = Logger.getLogger(FileTypeIconProvider.class.getName());
-    private static Pattern pattern;
-    private static Pattern fileNamePattern = Pattern.compile("/([^/]*?(\\.|-)(zip|rar|avi|wmv|mp3?|srt|sub|apk))\\.html?", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-    private static Pattern encoded = Pattern.compile("%[A-Z0-9]{2}%");
-    //    private static final String DEFAULT_EXTENSION = "unknown";
-    private final Map<String, Icon> systemLargeIcons = new Hashtable<String, Icon>();
-    private final Map<String, Icon> systemSmallIcons = new Hashtable<String, Icon>();
 
+    private final static Pattern fileExtensionPattern = Pattern.compile("[\\.\\-_]([a-z\\d]+?)$");
+    private final static Pattern fileNamePattern = Pattern.compile("/([^/]*?(\\.|-)(zip|rar|avi|wmv|mp3?|srt|sub|apk))\\.html?", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+    private final static Pattern encodedPattern = Pattern.compile("%[A-Z0-9]{2}%");
+
+    private final Map<String, Icon> systemIcons = new ConcurrentHashMap<String, Icon>();
+    private final ResourceMap resourceMap;
 
     public FileTypeIconProvider(ApplicationContext context) {
-        map = context.getResourceMap();
-        final String[] strings = (String[]) map.getObject("fileTypes", String[].class);
-        StringBuilder builder = new StringBuilder();
-        builder.append("(\\.|_)(");
-        for (int i = 0; i < strings.length; i++) {
-            String item = strings[i];
-            builder.append(item);
-            if (i != strings.length - 1) {
-                builder.append("|");
-            }
-        }
-        builder.append(")(\\.?|_?|$)");
-        final String regexp = builder.toString();
-        logger.info("Regexp " + regexp);
-        pattern = Pattern.compile(regexp);
+        resourceMap = context.getResourceMap();
     }
 
     public static String identifyFileType(String fileName) {
-        if (pattern == null)
-            throw new IllegalStateException("Not initialized yet");
-        if (fileName == null)
+        if (fileName == null) {
             return "";
-        fileName = fileName.toLowerCase();
-
-        final Matcher matcher = pattern.matcher(fileName);
-        final String fileType;
-        if (matcher.find()) {
-            fileType = matcher.group(2);
-        } else {
-            fileType = "";
         }
-
-        return fileType;
+        fileName = fileName.toLowerCase(Locale.ENGLISH);
+        final Matcher matcher = fileExtensionPattern.matcher(fileName);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            return "";
+        }
     }
 
     public static String identifyFileName(final String url) {
@@ -87,123 +67,89 @@ public class FileTypeIconProvider {
         return checkEncodedFileName(s);
     }
 
+    private static String checkEncodedFileName(String name) {
+        if (encodedPattern.matcher(name).find()) {
+            return Utils.urlDecode(name);
+        }
+        return name;
+    }
+
     public Icon getIconImageByFileType(String fileType, boolean bigImage) {
-        if (fileType == null)
+        if (fileType == null) {
             return null;
+        }
         if (AppPrefs.getProperty(UserProp.USE_SYSTEM_ICONS, true)) {
-            fileType = fileType.toLowerCase();
-
-            if (bigImage)
-                return getBigSystemIcon(fileType);
-            else
-                return getSmallSystemIcon(fileType);
-
+            fileType = fileType.toLowerCase(Locale.ENGLISH);
+            return getSystemIcon(fileType, bigImage);
         } else {
-
-
             fileType = fileType.toUpperCase(Locale.ENGLISH);
-
-
             final String base;
             if (bigImage) {
                 base = "iconFileTypeBig_" + fileType;
             } else {
                 base = "iconFileTypeSmall_" + fileType;
             }
-            if (map.containsKey(base)) {
-                return map.getImageIcon(base);
+            if (resourceMap.containsKey(base)) {
+                return resourceMap.getImageIcon(base);
             } else {
-                if (bigImage)
-                    return map.getImageIcon("iconFileTypeBig_ISO");
-                else
-                    return map.getImageIcon("iconFileTypeSmall_ISO");
+                if (bigImage) {
+                    return resourceMap.getImageIcon("iconFileTypeBig_ISO");
+                } else {
+                    return resourceMap.getImageIcon("iconFileTypeSmall_ISO");
+                }
             }
         }
     }
 
-    private Icon getSmallSystemIcon(String extension) {
-//        if ("".equals(extension))
-//            return null;
-        if (this.systemSmallIcons.containsKey(extension))
-            return systemSmallIcons.get(extension);
+    private Icon getSystemIcon(String extension, boolean bigImage) {
+        Icon icon = systemIcons.get(extension + bigImage);
+        if (icon != null) {
+            return icon;
+        }
         File file = null;
         try {
-//Create a temporary file with the specified extension
             file = File.createTempFile("icon", "." + extension);
-            if (file == null)
-                throw new IOException("Creation TMP file failed");
-            FileSystemView view = FileSystemView.getFileSystemView();
-            Icon icon = view.getSystemIcon(file);
-            if (icon == null) {
-                final ImageIcon ico = map.getImageIcon("iconFileTypeSmall_UNKNOWN");
-                systemSmallIcons.put(extension, ico);
-                return ico;
+            if (bigImage) {
+                icon = getShellFolderIcon(file);
+            } else {
+                icon = FileSystemView.getFileSystemView().getSystemIcon(file);
             }
-            //Delete the temporary file
-            systemSmallIcons.put(extension, icon);
-            return icon;
         } catch (IOException e) {
-            return map.getImageIcon("iconFileTypeSmall_UNKNOWN");
+            //ignore
         } catch (NullPointerException e) {//JDK BUG
-            return map.getImageIcon("iconFileTypeSmall_UNKNOWN");
+            //ignore
         } catch (ClassFormatError e) {
-            return map.getImageIcon("iconFileTypeSmall_UNKNOWN");
+            //ignore
         } finally {
-            if (file != null)
-                file.delete();
-        }
-    }
-
-    private Icon getBigSystemIcon(String extension) {
-        if (this.systemLargeIcons.containsKey(extension))
-            return systemLargeIcons.get(extension);
-        File file = null;
-        try {
-            file = File.createTempFile("icon", "." + extension);
-
-            Image ico = getShellFolderIcon(file);
-            if (ico == null) {
-                Icon icon = map.getImageIcon("iconFileTypeBig_UNKNOWN");
-                systemLargeIcons.put(extension, icon);
-                return icon;
+            if (file != null) {
+                if (!file.delete()) {
+                    logger.warning("Failed to delete temporary file " + file);
+                }
             }
-            Icon icon = new ImageIcon(ico);
-            //Delete the temporary file
-            systemLargeIcons.put(extension, icon);
-            return icon;
-        } catch (IOException e) {
-            return map.getImageIcon("iconFileTypeBig_UNKNOWN");
-        } finally {
-            if (file != null)
-                file.delete();
         }
+        if (icon == null) {
+            icon = resourceMap.getImageIcon("iconFileTypeSmall_UNKNOWN");
+        }
+        systemIcons.put(extension + bigImage, icon);
+        return icon;
     }
 
-    private static Image getShellFolderIcon(final File file) {
-//        ShellFolder shellFolder = ShellFolder.getShellFolder(file);
-//        Image ico = shellFolder.getIcon(true);
-
+    private static Icon getShellFolderIcon(final File file) {
+        //This method takes several milliseconds to run, but it's invoked rarely.
         try {
             final Method method = Class.forName("sun.awt.shell.ShellFolder").getMethod("getShellFolder", File.class);
             final Object sf = method.invoke(null, file);
-            final Method m = sf.getClass().getMethod("getIcon", Boolean.TYPE);
+            final Method m = sf.getClass().getMethod("getIcon", boolean.class);
             m.setAccessible(true);
             final Object result = m.invoke(sf, true);
-            return (Image) result;
+            if (result != null) {
+                return new ImageIcon((Image) result);
+            } else {
+                return null;
+            }
         } catch (Exception e) {
             return null;
         }
     }
-
-    private static String checkEncodedFileName(String name) {
-        if (encoded.matcher(name).find()) {
-            return Utils.urlDecode(name);
-        }
-        return name;
-    }
-
-    //    public static void main(String[] args) {
-//        assert identifyFileName("http://netload.in/dateiMTc4MzUxMT/heroes.311.hdtv-lol.avi.htm").equals("heroes.311.hdtv-lol.avi");
-//    }
 
 }
