1757bd78caf5f282964534ab2a73e1681976a90c
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 07:12:53 +0530

    SVN Update# 2545 by ntoskrnl: moved RTMP library to a plugin

--- src/cz/vity/freerapid/plugins/webclient/AbstractRunner.java
+++ src/cz/vity/freerapid/plugins/webclient/AbstractRunner.java
@@ -1,14 +1,9 @@
 package cz.vity.freerapid.plugins.webclient;
 
 import cz.vity.freerapid.plugins.exceptions.BuildMethodException;
-import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
 import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.interfaces.*;
-import cz.vity.freerapid.plugins.webclient.rtmp.RtmpClient;
-import cz.vity.freerapid.plugins.webclient.rtmp.RtmpSession;
-import cz.vity.freerapid.plugins.webclient.utils.HttpUtils;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
-import cz.vity.freerapid.utilities.LogUtils;
 import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
@@ -161,59 +156,6 @@ public abstract class AbstractRunner implements PluginRunner {
         } finally {
             method.abort();
             method.releaseConnection();
-        }
-    }
-
-    /**
-     * Method uses given RtmpSession parameter to connect to the server and tries to download.<br />
-     * Download state of HttpFile is updated automatically - sets <code>DownloadState.GETTING</code> and then <code>DownloadState.DOWNLOADING</code>.
-     * The HttpClient parameter <code>noContentLengthAvailable</code> is also set.
-     *
-     * @param rtmpSession RtmpSession to use for downloading
-     * @return true if file was successfully downloaded, false otherwise
-     * @throws Exception if something goes horribly wrong
-     * @see RtmpSession
-     */
-    protected boolean tryDownloadAndSaveFile(final RtmpSession rtmpSession) throws Exception {
-        httpFile.setState(DownloadState.GETTING);
-        logger.info("Starting RTMP download");
-
-        httpFile.getProperties().remove(DownloadClient.START_POSITION);
-        httpFile.getProperties().remove(DownloadClient.SUPPOSE_TO_DOWNLOAD);
-
-        final String fn = httpFile.getFileName();
-        if (fn == null || fn.isEmpty())
-            throw new IOException("No defined file name");
-        httpFile.setFileName(HttpUtils.replaceInvalidCharsForFileSystem(PlugUtils.unescapeHtml(fn), "_"));
-
-        client.getHTTPClient().getParams().getBooleanParameter("noContentLengthAvailable", true);
-
-        RtmpClient rtmpClient = null;
-        try {
-            rtmpClient = new RtmpClient(rtmpSession);
-            rtmpClient.connect();
-
-            InputStream in = rtmpClient.getStream();
-
-            if (in != null) {
-                logger.info("Saving to file");
-                downloadTask.saveToFile(in);
-                return true;
-            } else {
-                logger.info("Saving file failed");
-                return false;
-            }
-        } catch (Throwable e) {
-            throw new PluginImplementationException("RTMP error (" + e.toString() + ")", e);
-        } finally {
-            if (rtmpClient != null) {
-                try {
-                    //no need to specifically close any streams, this method handles that too
-                    rtmpClient.disconnect();
-                } catch (Exception e) {
-                    LogUtils.processException(logger, e);
-                }
-            }
         }
     }
 
