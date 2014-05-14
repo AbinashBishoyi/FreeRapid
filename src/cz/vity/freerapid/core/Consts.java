a90c5467d82192a43db3162b49f2b320542cc3d7
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 10:15:22 +0530

    SVN Update# 4685 by Vity: forgot to add consts :-(

--- src/cz/vity/freerapid/core/Consts.java
+++ src/cz/vity/freerapid/core/Consts.java
@@ -15,7 +15,7 @@ public class Consts {
     /**
      * Version
      */
-    public static final String VERSION = "0.9u2";
+    public static final String VERSION = "0.9u3";
 
     /**
      * verze a jmeno programu
