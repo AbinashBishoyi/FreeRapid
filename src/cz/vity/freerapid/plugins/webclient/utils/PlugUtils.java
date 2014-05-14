ee9c99c437f77040b8a2fd2566b0ad1cbaa0cd3d
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 08:10:28 +0530

    SVN Update# 3117 by Vity: support for \\uAAAA decoder

--- src/cz/vity/freerapid/plugins/webclient/utils/PlugUtils.java
+++ src/cz/vity/freerapid/plugins/webclient/utils/PlugUtils.java
@@ -139,6 +139,139 @@ public final class PlugUtils {
         }
     }
 
+
+    /**
+     * Method converts a string in this form:<br>
+     * <code>video_title", "\u0025D9\u002583\u0025D8\u0025B4\u0025D9\u002581+\u0025D8\u0025A7\u0025D9\u002584\u0025D8\u0025AD\u0025D9\u002582\u0025D9\u00258A\u0025D9\u002582\u0025D8\u0025A9+32++\u0025D8\u0025A7\u0025D9\u002584\u0025D8\u0025AA\u0025D9\u002588\u0025D8\u0025B6\u0025D9\u00258A\u0025D8\u0025AD</code>
+     * <br> into <code>Slečna si to opravdu užívá... _))))))))))))))))))</code><br />
+     * @param str a string to convert
+     * @return decoded string
+     * @since 0.86
+     * @throws IllegalArgumentException if malformed \\u encoding
+     */
+    public static String unescapeUnicode(String str) {
+        StringBuilder buf = new StringBuilder();
+        int i = 0;
+        for (int len = str.length(); i < len; i++) {
+            char c = str.charAt(i);
+            label0:
+            switch (c) {
+                case 92: // '\\'
+                    if (i == str.length() - 1) {
+                        buf.append('\\');
+                        break;
+                    }
+                    c = str.charAt(++i);
+                    switch (c) {
+                        case 110: // 'n'
+                            buf.append('\n');
+                            break label0;
+
+                        case 116: // 't'
+                            buf.append('\t');
+                            break label0;
+
+                        case 114: // 'r'
+                            buf.append('\r');
+                            break label0;
+
+                        case 117: // 'u'
+                            int value = 0;
+                            for (int j = 0; j < 4; j++) {
+                                c = str.charAt(++i);
+                                switch (c) {
+                                    case 48: // '0'
+                                    case 49: // '1'
+                                    case 50: // '2'
+                                    case 51: // '3'
+                                    case 52: // '4'
+                                    case 53: // '5'
+                                    case 54: // '6'
+                                    case 55: // '7'
+                                    case 56: // '8'
+                                    case 57: // '9'
+                                        value = ((value << 4) + c) - 48;
+                                        break;
+
+                                    case 97: // 'a'
+                                    case 98: // 'b'
+                                    case 99: // 'c'
+                                    case 100: // 'd'
+                                    case 101: // 'e'
+                                    case 102: // 'f'
+                                        value = ((value << 4) + 10 + c) - 97;
+                                        break;
+
+                                    case 65: // 'A'
+                                    case 66: // 'B'
+                                    case 67: // 'C'
+                                    case 68: // 'D'
+                                    case 69: // 'E'
+                                    case 70: // 'F'
+                                        value = ((value << 4) + 10 + c) - 65;
+                                        break;
+
+                                    case 58: // ':'
+                                    case 59: // ';'
+                                    case 60: // '<'
+                                    case 61: // '='
+                                    case 62: // '>'
+                                    case 63: // '?'
+                                    case 64: // '@'
+                                    case 71: // 'G'
+                                    case 72: // 'H'
+                                    case 73: // 'I'
+                                    case 74: // 'J'
+                                    case 75: // 'K'
+                                    case 76: // 'L'
+                                    case 77: // 'M'
+                                    case 78: // 'N'
+                                    case 79: // 'O'
+                                    case 80: // 'P'
+                                    case 81: // 'Q'
+                                    case 82: // 'R'
+                                    case 83: // 'S'
+                                    case 84: // 'T'
+                                    case 85: // 'U'
+                                    case 86: // 'V'
+                                    case 87: // 'W'
+                                    case 88: // 'X'
+                                    case 89: // 'Y'
+                                    case 90: // 'Z'
+                                    case 91: // '['
+                                    case 92: // '\\'
+                                    case 93: // ']'
+                                    case 94: // '^'
+                                    case 95: // '_'
+                                    case 96: // '`'
+                                    default:
+                                        throw new IllegalArgumentException("Malformed \\uxxxx encoding.");
+                                }
+                            }
+
+                            buf.append((char) value);
+                            break;
+
+                        case 111: // 'o'
+                        case 112: // 'p'
+                        case 113: // 'q'
+                        case 115: // 's'
+                        default:
+                            buf.append(c);
+                            break;
+                    }
+                    break;
+
+                default:
+                    buf.append(c);
+                    break;
+            }
+        }
+
+        return buf.toString();
+    }
+
+
     /**
      * <p>Find and return value of given parameter in html tag (eg. input).</p>
      * <p/>
