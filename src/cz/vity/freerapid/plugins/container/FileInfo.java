f54cd705458cab5ed75ae0ccf43adb3e487f5769
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 08:07:48 +0530

    SVN Update# 3082 by Stan: QueueSupport - two useful constructors

--- src/cz/vity/freerapid/plugins/container/FileInfo.java
+++ src/cz/vity/freerapid/plugins/container/FileInfo.java
@@ -19,6 +19,35 @@ public class FileInfo {
         this.fileUrl = fileUrl;
     }
 
+    /**
+     * Constructs dto with informations about file which will be added to download list
+     * @param fileUrl url where locates file
+     * @param fileName with this name will be file saved
+     * @param fileSize file size in Bytes
+     * @since 0.852
+     */
+    public FileInfo(URL fileUrl, String fileName, long fileSize) {
+        this(fileUrl, fileName, fileSize, null);
+    }
+
+    /**
+     * Constructs dto with informations about file which will be added to download list
+     * @param fileUrl url where locates file
+     * @param fileName with this name will be file saved
+     * @param fileSize file size in Bytes
+     * @param description any description of file - not necessary
+     * @since 0.852
+     */
+    public FileInfo(URL fileUrl, String fileName, long fileSize, String description) {
+        if (fileUrl == null) {
+            throw new IllegalArgumentException("fileUrl == null");
+        }
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.description = description;
+    }
+
     public URL getFileUrl() {
         return fileUrl;
     }
@@ -46,5 +75,4 @@ public class FileInfo {
     public void setDescription(final String description) {
         this.description = description;
     }
-
 }
