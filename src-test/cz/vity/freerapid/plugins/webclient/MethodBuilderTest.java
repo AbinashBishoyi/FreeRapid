785fbdf44b7d93534c2888f1d5129e949882e528
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 05:48:52 +0530

    SVN Update# 1573 by Vity: simple form by name test

--- src-test/cz/vity/freerapid/plugins/webclient/MethodBuilderTest.java
+++ src-test/cz/vity/freerapid/plugins/webclient/MethodBuilderTest.java
@@ -31,7 +31,7 @@ public class MethodBuilderTest {
 
     @Test
     public void testSetActionFromAHrefWhereATagContains() throws BuildMethodException {
-        final MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        final MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromAHrefWhereATagContains("Download file").getAction();
         Assert.assertEquals("Parsing A tag", "http://dl1u.savefile.com/aca4c18942687b57f454185120bb2f45/plugins.zip", action);
         action = methodBuilder.setActionFromAHrefWhereATagContains("Your Ad").getAction();
@@ -55,7 +55,7 @@ public class MethodBuilderTest {
 
     @Test
     public void testSetActionFromFormWhereTagContains() throws BuildMethodException, URIException {
-        MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromFormWhereTagContains("Zaregistruj se", false).getAction();
         Assert.assertEquals("Parsing Form tag", "/login/", action);
 
@@ -69,7 +69,7 @@ public class MethodBuilderTest {
         String s = methodBuilder.setBaseURL("http://testbase.com/").toGetMethod().getURI().toString();
         Assert.assertEquals("Parsing Form tag", "http://testbase.com/login/", s);
 
-        methodBuilder = new MethodBuilder(content, client);
+        methodBuilder = getMethodBuilder();
         methodBuilder.setActionFromFormWhereTagContains("Zaregistruj se", true);
         s = methodBuilder.setBaseURL("http://testbase.com/").toGetMethod().getURI().toString();
         Assert.assertEquals("Parsing Form tag", "http://testbase.com/login/?login=&pass=&pamatovat=1&prihlasit=P%C5%99ihl%C3%A1sit", s);
@@ -79,28 +79,28 @@ public class MethodBuilderTest {
 
     @Test
     public void testSetActionFromFormWhereActionContains() throws BuildMethodException {
-        final MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        final MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromFormWhereActionContains("login", true).getAction();
         Assert.assertEquals("Parsing Form tag", "/login/", action);
     }
 
     @Test
     public void testSetActionFromFormByIndex() throws BuildMethodException {
-        final MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        final MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromFormByIndex(3, true).getAction();
         Assert.assertEquals("Parsing Form tag", "/login/", action);
     }
 
     @Test
     public void testSetActionFromFormByName() throws BuildMethodException {
-        MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromFormByName("test", true).getAction();
         Assert.assertEquals("Parsing Form tag", "/login/", action);
 
         action = methodBuilder.setActionFromFormByName("F1", true).getAction();
         Assert.assertEquals("Parsing Form tag", "./testAction/", action);
 
-        methodBuilder = new MethodBuilder(content, client);
+        methodBuilder = getMethodBuilder();
         action = methodBuilder.setActionFromFormByName("downForm", true).getAction();
         Assert.assertEquals("Parsing Form tag", "http://www.badongo.com/cfile/8203387", action);
 
@@ -111,11 +111,19 @@ public class MethodBuilderTest {
         Assert.assertEquals("Correct parameter value", "269200932", params.get("cap_id"));
         Assert.assertEquals("Correct parameter value", "281f3fe24cc4894a5bb61e4a6b12a3b7", params.get("cap_secret"));
         Assert.assertEquals("Correct parameter value", "", params.get("user_code")); //parametr nema hodnotu
+
+        methodBuilder = getMethodBuilder();
+        action = methodBuilder.setActionFromFormByName("f", true).getAction();
+        Assert.assertEquals("Simple Form name", "/dl/12662/9832a7e/P_hpaf.rar.html", action);
+    }
+
+    private MethodBuilder getMethodBuilder() {
+        return new MethodBuilder(content, client);
     }
 
     @Test
     public void testSetActionFromTextBetween() throws BuildMethodException {
-        final MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        final MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromTextBetween("$ld('", "', '").getAction();
         String resultLink = "/process.php?process=get_link&id=300e73bd1792&tm=1236436294&th=a555de272268aa132a9d57e793e09fe9&ur=0";
         Assert.assertEquals("Searched text between text", resultLink, action);
@@ -124,7 +132,7 @@ public class MethodBuilderTest {
 
     @Test
     public void testSetActionFromAHrefWhereImgTagContains() throws BuildMethodException {
-        final MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        final MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromImgSrcWhereTagContains("/captchas/").getAction();
         String resultLink = "http://bagruj.cz/captchas/wp2fjkdzvlndpf6j1ev9.jpg";
         Assert.assertEquals("Searched IMG tag text in the tag", resultLink, action);
@@ -133,7 +141,7 @@ public class MethodBuilderTest {
 
     @Test
     public void testEncodeLastPartOfAction() throws BuildMethodException {
-        final MethodBuilder methodBuilder = new MethodBuilder(content, client);
+        final MethodBuilder methodBuilder = getMethodBuilder();
         String action = methodBuilder.setActionFromFormByName("downForm2", true).encodeLastPartOfAction().getAction();
         String resultLink = "http://www.badongo.com/cfile/%C4%8Cesk%C3%A9+z%C3%A1zem%C3%AD/";
         Assert.assertEquals("Encoded last part of action with /", resultLink, action);
