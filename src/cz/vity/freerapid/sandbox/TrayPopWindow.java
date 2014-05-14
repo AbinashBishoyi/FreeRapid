0b20507ffc1fb4027390428f511aa5d5c33f6d70
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-05-14 09:40:40 +0530

    SVN Update# 4200 by Vity: tray update

--- src/cz/vity/freerapid/sandbox/TrayPopWindow.java
+++ src/cz/vity/freerapid/sandbox/TrayPopWindow.java
@@ -1,9 +1,12 @@
 package cz.vity.freerapid.sandbox;
 
+import com.jgoodies.forms.factories.CC;
+import com.jgoodies.forms.layout.FormLayout;
 import org.jdesktop.swingx.border.DropShadowBorder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 
 /**
  * @author Vity
@@ -11,6 +14,9 @@ import java.awt.*;
 public class TrayPopWindow extends JWindow {
 
     private JLabel textLabel;
+    private JButton btnBack;
+    private JButton btnForward;
+    private JButton btnClose;
 
     public TrayPopWindow() {
         super();
@@ -19,16 +25,47 @@ public class TrayPopWindow extends JWindow {
     }
 
     private void init() {
-        setPreferredSize(new Dimension(200, 80));
+        setPreferredSize(new Dimension(300, 140));
         setSize(getPreferredSize());
+        final JPanel contentPane = new JPanel();
+
+        this.setContentPane(contentPane);
+        this.setShape(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 2, 4));
         this.setLayout(new BorderLayout());
-        final JLabel freeRapid = new JLabel("FreeRapid");
-        freeRapid.setBorder(new DropShadowBorder(Color.BLACK, 2, 0.5f, 2, true, true, false, true));
-        freeRapid.setBackground(Color.GREEN);
-        this.add(freeRapid, BorderLayout.NORTH);
-        freeRapid.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+        final JPanel titlePanel = new JPanel();
+        JLabel labelIcon = new JLabel();
+        btnBack = new JButton();        
+        btnForward = new JButton();
+        btnClose = new JButton();
+        btnClose.setBorderPainted(false);
+
+        titlePanel.setLayout(new FormLayout(
+        			"default, $lcgap, default:grow, 2*($lcgap, center:default), $lcgap, default:grow, $lcgap, default",
+        			"default"));
+        contentPane.setBorder(new DropShadowBorder(Color.BLACK, 4, 0.5f, 4, true, true, true, true));
+
+        titlePanel.setBorder(BorderFactory.createCompoundBorder(new DropShadowBorder(Color.BLACK, 2, 0.3f, 4, false, false, true, false), BorderFactory.createEmptyBorder(4, 4, 4, 4)));
+
+        //---- labelIcon ----
+        labelIcon.setText("text");
+        titlePanel.add(labelIcon, CC.xy(1, 1));
+
+        //---- btnBack ----
+        btnBack.setText("text");
+        titlePanel.add(btnBack, CC.xy(5, 1));
+
+        //---- btnForward ----
+        btnForward.setText("text");
+        titlePanel.add(btnForward, CC.xy(7, 1));
+
+        //---- btnClose ----
+        btnClose.setText("text");
+        titlePanel.add(btnClose, CC.xy(11, 1));
+
+
+        this.add(titlePanel, BorderLayout.NORTH);
         final JPanel panel = new JPanel(new BorderLayout());
-        panel.setBorder(BorderFactory.createCompoundBorder(new DropShadowBorder(Color.BLACK, 4, 0.5f, 4, true, true, true, true), BorderFactory.createEmptyBorder(2, 2, 2, 2)));
+        panel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
         this.add(panel, BorderLayout.CENTER);
         textLabel = new JLabel();
         panel.add(textLabel, BorderLayout.CENTER);
@@ -44,6 +81,31 @@ public class TrayPopWindow extends JWindow {
 
     public void setY(int y) {
         setLocation(getLocation().x, y);
+    }
+
+    public JButton getBtnClose() {
+        return btnClose;
+    }
+
+    private AbstractButton getButton(final Action action) {
+        final JButton button = new JButton(action);
+        return setButtonProperties(button, action);
+    }
+
+
+    private AbstractButton setButtonProperties(AbstractButton button, Action action) {
+        button.setRolloverEnabled(true);
+        button.setIconTextGap(0);
+        final Object desc = action.getValue(Action.SHORT_DESCRIPTION);
+
+        final Object keystroke = action.getValue(Action.ACCELERATOR_KEY);
+        if (desc != null) {
+            button.setToolTipText(desc.toString());
+        }
+
+        button.setMnemonic(0);
+        button.setFocusable(false);
+        return button;
     }
 
 }
--- src/cz/vity/freerapid/sandbox/TrayPopWindowReporter.java
+++ src/cz/vity/freerapid/sandbox/TrayPopWindowReporter.java
@@ -13,9 +13,15 @@ import java.awt.event.MouseEvent;
 /**
  * @author Vity
  */
-public class TrayTest {
+public class TrayPopWindowReporter {
 
     private static final float MAX_OPACITY = 0.9f;
+    private Timer waitTimer;
+    private enum PopWindowState {
+        OPENING, SHOWING, CLOSING, NONE
+    }
+    private PopWindowState state = PopWindowState.NONE;
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -32,7 +38,7 @@ public class TrayTest {
                 } catch (UnsupportedLookAndFeelException e) {
                     e.printStackTrace();
                 }
-                new TrayTest().start();
+                new TrayPopWindowReporter().start();
             }
         });
     }
@@ -45,32 +51,49 @@ public class TrayTest {
         fadeIn(window, 1500);
     }
 
-    public static void fadeIn(final Window window, final int fadeOutDuration) {
+    public void fadeIn(final TrayPopWindow window, final int fadeInDuration) {
+        state = PopWindowState.OPENING;
         window.setOpacity(0);
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         final Rectangle bounds = ge.getMaximumWindowBounds();
         final int startY = (int) (bounds.getY() + bounds.getHeight());
         window.setLocation((int) (bounds.getX() + bounds.getWidth() - window.getSize().width) - 2, startY);
         window.setVisible(true);
-        final Timer waitTimer = new Timer(2000, new ActionListener() {
+        final ActionListener listener = new ActionListener() {
+
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                fadeOut(window, fadeOutDuration);
-            }
-        });
-        waitTimer.setRepeats(false);
-
-        Timeline dispose = new Timeline(window);
-        final MouseInputAdapter delayMouseAdapter = new MouseInputAdapter() {
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                //some optimization needed
-                if (waitTimer.isRunning()) {
+                if (window.getMousePosition(true) != null) {
                     waitTimer.restart();
+                    return;
+                }
+                if (state == PopWindowState.SHOWING) {
+                    fadeOut(window, fadeInDuration * 2 / 3);
                 }
             }
         };
-        window.addMouseMotionListener(delayMouseAdapter);
+
+        window.getBtnClose().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (state == PopWindowState.SHOWING) {
+                    fadeOut(window, fadeInDuration / 8);
+                }
+            }
+        });
+
+        waitTimer = new Timer(2000, listener);
+        waitTimer.setRepeats(false);
+
+        Timeline dispose = new Timeline(window);
+
+        window.addMouseMotionListener(new MouseInputAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+            }
+        });
 
         dispose.addPropertyToInterpolate("opacity", 0.0f, MAX_OPACITY);
         dispose.addPropertyToInterpolate("y", startY, startY - window.getHeight() - 20);
@@ -80,20 +103,20 @@ public class TrayTest {
                                                Timeline.TimelineState newState, float durationFraction,
                                                float timelinePosition) {
                 if (newState == Timeline.TimelineState.DONE) {
+                    state = PopWindowState.SHOWING;
                     waitTimer.start();
                 }
             }
         });
-        dispose.setDuration(fadeOutDuration);
+        dispose.setDuration(fadeInDuration);
         dispose.play();
     }
 
-    private static void fadeOut(final Window window, final int fadeOutDuration) {
-
+    private void fadeOut(final Window window, final int fadeOutDuration) {
+        this.state = PopWindowState.CLOSING;
         final Timeline dispose = new Timeline(window);
-
-        dispose.addPropertyToInterpolate("opacity",MAX_OPACITY, 0.0f);
-        dispose.addPropertyToInterpolate("y", window.getY(), window.getY() + window.getHeight() + 20);
+        dispose.addPropertyToInterpolate("opacity", MAX_OPACITY, 0.0f);
+       // dispose.addPropertyToInterpolate("y", window.getY(), window.getY() + window.getHeight() + 20);
         dispose.addCallback(new UIThreadTimelineCallbackAdapter() {
             @Override
             public void onTimelineStateChanged(Timeline.TimelineState oldState,
@@ -102,6 +125,7 @@ public class TrayTest {
                 if (newState == Timeline.TimelineState.DONE) {
                     window.setVisible(false);
                     window.dispose();
+                    state = PopWindowState.NONE;
                 }
             }
         });
